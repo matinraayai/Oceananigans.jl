209873b17dcab141b0297fc409c1c6676a0aac6e
Gregory Wagner <wagner.greg@gmail.com>
2021-05-24 21:09:03 -0800

    Fixes a bug in hydrostatic pressure update for IncompressibleModel

--- src/Models/IncompressibleModels/update_hydrostatic_pressure.jl
+++ src/Models/IncompressibleModels/update_hydrostatic_pressure.jl
@@ -12,6 +12,6 @@ the `buoyancy_perturbation` downwards:
     @inbounds pHY′[i, j, grid.Nz] = - ℑzᵃᵃᶠ(i, j, grid.Nz+1, grid, z_dot_g_b, buoyancy, C) * Δzᵃᵃᶜ(i, j, grid.Nz+1, grid)
 
     @unroll for k in grid.Nz-1 : -1 : 1
-        @inbounds pHY′[i, j, k] = pHY′[i, j, k+1] - ℑzᵃᵃᶠ(i, j, k+1, grid, z_dot_g_b, buoyancy, C) * Δzᵃᵃᶜ(i, j, k+1, grid)
+        @inbounds pHY′[i, j, k] = pHY′[i, j, k+1] - ℑzᵃᵃᶠ(i, j, k+1, grid, z_dot_g_b, buoyancy, C) * Δzᵃᵃᶠ(i, j, k+1, grid)
     end
 end
