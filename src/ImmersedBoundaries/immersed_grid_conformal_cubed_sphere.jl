632a6e3725c36dae0c3b239a270b45117b0763f7
christophernhill <cnh@mit.edu>
2021-05-29 23:21:51 -0400

    Trying to make IBG and Cube containers play nice.  - Getting recursive function loop, not sure why!

--- src/ImmersedBoundaries/immersed_grid_conformal_cubed_sphere.jl
+++ src/ImmersedBoundaries/immersed_grid_conformal_cubed_sphere.jl
@@ -9,14 +9,22 @@ function new_data(FT, arch, ibg::ImmersedBoundaryGrid{F,B,B,B,T}, loc ) where {T
          return new_data(FT, arch, ibg.grid, loc )
 end     
 
-### function total_size(loc, ibg::ImmersedBoundaryGrid{F,B,B,B,T} ) where {T<:ConformalCubedSphereGrid,B<:Any,F<:Real}
-###         return total_size(loc, ibg.grid)
-### end
-
-
 import Oceananigans.Fields: validate_field_data
 
 function validate_field_data(X, Y, Z, data, ibg::ImmersedBoundaryGrid{F,B,B,B,T} ) where {T<:ConformalCubedSphereGrid,B<:Any,F<:Real}
    validate_field_data(X, Y, Z, data, ibg.grid)
 end
 
+import Oceananigans.CubedSpheres: faces
+import Oceananigans.CubedSpheres: get_face
+
+function faces(field::Oceananigans.Fields.ReducedField{<:Any,<:Any,<:Any,<:Any,<:Any,<:ImmersedBoundaryGrid,<:Any,<:Any,<:Any})
+        println("Get faces for an IBG grid cube sphere field.")
+        println("Field type is ",typeof(field))
+        Tuple(get_face(field, face_index) for face_index in 1:length(field.data.faces))
+end
+
+function get_face(field::ImmersedBoundaryGrid, face_index)
+        println("Extracting grid from IBG wrapper")
+        get_face(field.grid,face_index)
+end
