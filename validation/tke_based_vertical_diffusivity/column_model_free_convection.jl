3343ab634c813e7e32d1d31528f5237c7346ef42
Gregory Wagner <wagner.greg@gmail.com>
2021-06-04 09:06:29 -0800

    Updates free convection validation experiment

--- validation/tke_based_vertical_diffusivity/column_model_free_convection.jl
+++ validation/tke_based_vertical_diffusivity/column_model_free_convection.jl
@@ -5,53 +5,69 @@ using Oceananigans.Units
 using Oceananigans.TurbulenceClosures: VerticallyImplicitTimeDiscretization, TKEBasedVerticalDiffusivity
 using Oceananigans.TurbulenceClosures: RiDependentDiffusivityScaling
 
-grid = RegularRectilinearGrid(size=32, z=(-64, 0), topology=(Flat, Flat, Bounded))
+function run_free_convection(; Nz=32, stop_time=48hours, Qᵇ = 1e-8)
 
-closure = TKEBasedVerticalDiffusivity(time_discretization=VerticallyImplicitTimeDiscretization())
-                                      
-Qᵇ = 1e-8
-Qᵘ = 0.0
-Qᵛ = 0.0
+    grid = RegularRectilinearGrid(size=Nz, z=(-128, 0), topology=(Flat, Flat, Bounded))
 
-u★ = (Qᵘ^2 + Qᵛ^2)^(1/4)
-w★ = Qᵇ * grid.Δz
+    closure = TKEBasedVerticalDiffusivity()
+    b_bcs = TracerBoundaryConditions(grid; top = FluxBoundaryCondition(Qᵇ))
 
-Qᵉ = - closure.dissipation_parameter * (closure.surface_model.CᵂwΔ * w★ + closure.surface_model.Cᵂu★ * u★)
+    model = HydrostaticFreeSurfaceModel(grid = grid,
+                                        tracers = (:b, :e),
+                                        buoyancy = BuoyancyTracer(),
+                                        boundary_conditions = (b=b_bcs,),
+                                        closure = closure)
+                                        
+    N² = 1e-5
+    bᵢ(x, y, z) = N² * z
+    set!(model, b = bᵢ)
+                      
+    simulation = Simulation(model, Δt = 10.0, stop_time = 48hours)
 
-u_bcs = UVelocityBoundaryConditions(grid; top = FluxBoundaryCondition(Qᵘ))
-v_bcs = VVelocityBoundaryConditions(grid; top = FluxBoundaryCondition(Qᵛ))
-b_bcs = TracerBoundaryConditions(grid; top = FluxBoundaryCondition(Qᵇ))
-tke_bcs = TracerBoundaryConditions(grid; top = FluxBoundaryCondition(Qᵉ))
+    run!(simulation)
 
-model = HydrostaticFreeSurfaceModel(grid = grid,
-                                    tracers = (:b, :e),
-                                    buoyancy = BuoyancyTracer(),
-                                    boundary_conditions = (b=b_bcs, e=tke_bcs),
-                                    closure = closure)
-                                    
-N² = 1e-5
-bᵢ(x, y, z) = N² * z
-set!(model, b = bᵢ)
+    return simulation
+end
 
-z = znodes(model.tracers.b)
+function plot_free_convection!(simulation, plots=nothing; label="", color=:blue)
 
-b = view(interior(model.tracers.b), 1, 1, :)
-e = view(interior(model.tracers.e), 1, 1, :)
-Kc = view(interior(model.diffusivities.Kᶜ), 1, 1, :)
-Ke = view(interior(model.diffusivities.Kᵉ), 1, 1, :)
+    model = simulation.model
 
-b_plot = plot(b, z, linewidth = 2, label = "t = 0", xlabel = "Buoyancy", ylabel = "z", legend=:bottomright)
-e_plot = plot(e, z, linewidth = 2, label = "t = 0", xlabel = "TKE", ylabel = "z", legend=:bottomright)
-              
-simulation = Simulation(model, Δt = 20.0, stop_time = 48hours)
+    z = znodes(model.tracers.b)
+    b = view(interior(model.tracers.b), 1, 1, :)
+    e = view(interior(model.tracers.e), 1, 1, :)
+    Kc = view(interior(model.diffusivities.Kᶜ), 1, 1, :)
+    Ke = view(interior(model.diffusivities.Kᵉ), 1, 1, :)
 
-run!(simulation)
+    common_kwargs = (color = color, legend = :bottomright, ylabel = "z (m)")
 
-plot!(b_plot, b, z, linewidth = 2, label = @sprintf("t = %s", prettytime(model.clock.time)))
-plot!(e_plot, e, z, linewidth = 2, label = @sprintf("t = %s", prettytime(model.clock.time)))
-K_plot = plot(Kc, z, linewidth = 2, linestyle=:dash, label = @sprintf("Kᶜ, t = %s", prettytime(model.clock.time)), legend=:bottomright, xlabel="Diffusivities")
-plot!(K_plot, Ke, z, linewidth = 3, alpha=0.6, label = @sprintf("Kᵉ, t = %s", prettytime(model.clock.time)))
 
-eb_plot = plot(b_plot, e_plot, K_plot, layout=(1, 3), size=(1200, 600))
+    if plots === nothing
+        b_plot = plot(b,  z, linewidth = 2, label = @sprintf("%s t = %s",     label, prettytime(model.clock.time)), xlabel="Buoyancy", common_kwargs...)
+        e_plot = plot(e,  z, linewidth = 2, label = @sprintf("%s t = %s",     label, prettytime(model.clock.time)), xlabel="TKE", common_kwargs...)
+        K_plot = plot(Kc, z, linewidth = 2, label = @sprintf("%s Kᶜ, t = %s", label, prettytime(model.clock.time)), xlabel="Diffusivities", linestyle=:dash, common_kwargs...)
+        plot!(K_plot, Ke, z, linewidth = 3, label = @sprintf("%s Kᵉ, t = %s", label, prettytime(model.clock.time)), alpha=0.6, common_kwargs...)
 
-display(eb_plot)
+        return b_plot, e_plot, K_plot
+    else
+        plot!(plots.b, b, z,  linewidth = 2, color = color, label = @sprintf("%s t = %s", label, prettytime(model.clock.time)))
+        plot!(plots.e, e, z,  linewidth = 2, color = color, label = @sprintf("%s t = %s", label, prettytime(model.clock.time)))
+        plot!(plots.K, Kc, z, linewidth = 2, color = color, label = @sprintf("%s Kᶜ, t = %s", label, prettytime(model.clock.time)), linestyle=:dash)
+        plot!(plots.K, Ke, z, linewidth = 3, color = color, label = @sprintf("%s Kᵉ, t = %s", label, prettytime(model.clock.time)), alpha=0.6)
+
+        return nothing
+    end
+end
+
+b_plot, e_plot, K_plot = plot_free_convection!(run_free_convection(Nz=8), label="Nz=8", color=:blue)
+
+plots = (b=b_plot, e=e_plot, K=K_plot)
+
+plot_free_convection!(run_free_convection(Nz=8),  plots, label="Nz=8",  color=:green)
+plot_free_convection!(run_free_convection(Nz=32),  plots, label="Nz=32",  color=:cyan)
+plot_free_convection!(run_free_convection(Nz=64),  plots, label="Nz=64",  color=:red)
+plot_free_convection!(run_free_convection(Nz=128), plots, label="Nz=128", color=:black)
+
+summary = plot(b_plot, e_plot, K_plot, layout=(1, 3), size=(1200, 600))
+
+display(summary)
