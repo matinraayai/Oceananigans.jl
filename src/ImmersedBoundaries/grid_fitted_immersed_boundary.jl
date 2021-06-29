f2487280b36371a8a646eef66912f009f0a8f72e
Gregory Wagner <wagner.greg@gmail.com>
2021-06-29 10:25:33 -0600

    Adds two types to GridFittedBoundary to control viscous/diffusive conditional fluxes

--- src/ImmersedBoundaries/grid_fitted_immersed_boundary.jl
+++ src/ImmersedBoundaries/grid_fitted_immersed_boundary.jl
@@ -7,12 +7,18 @@ const ATD = AbstractTimeDiscretization
 
 struct RasterDepthMask end
 
-struct GridFittedBoundary{M, S} <: AbstractImmersedBoundary
+struct GridFittedBoundary{M, S, U, C} <: AbstractImmersedBoundary
     mask :: S
     mask_type :: M
+    immersed_viscous_momentum_fluxes :: U
+    immersed_diffusive_tracer_fluxes :: C
 end
 
-GridFittedBoundary(mask; mask_type=nothing) = GridFittedBoundary(mask, mask_type)
+GridFittedBoundary(mask;
+                   mask_type=nothing,
+                   immersed_viscous_momentum_fluxes=nothing,
+                   immersed_tracer_momentum_fluxes=nothing) = GridFittedBoundary(mask, mask_type, immersed_viscous_momentum_fluxes, immersed_diffusive_tracer_fluxes)
+                  
 
 @inline is_immersed(i, j, k, underlying_grid, ib::GridFittedBoundary) = ib.mask(node(c, c, c, i, j, k, underlying_grid)...)
 @inline is_immersed(i, j, k, underlying_grid, ib::GridFittedBoundary{<:RasterDepthMask}) = ib.mask(i, j)
@@ -51,29 +57,32 @@ const GFIBG = ImmersedBoundaryGrid{FT, TX, TY, TZ, G, <:GridFittedBoundary} wher
 @inline conditional_flux_cfc(i, j, k, ibg::IBG{FT}, flux, args...) where FT = ifelse(solid_node(c, f, c, i, j, k, ibg), zero(FT), flux(i, j, k, ibg, args...))
 @inline conditional_flux_ccf(i, j, k, ibg::IBG{FT}, flux, args...) where FT = ifelse(solid_node(c, c, f, i, j, k, ibg), zero(FT), flux(i, j, k, ibg, args...))
 
+const NoViscousFluxGFIBG = ImmersedBoundaryGrid{FT, TX, TY, TZ, G, <:GridFittedBoundary{<:Any, <:Any, ::Nothing}} where {FT, TX, TY, TZ, G}
+const NoDiffusiveFluxGFIBG = ImmersedBoundaryGrid{FT, TX, TY, TZ, G, <:GridFittedBoundary{<:Any, <:Any, <:Any, ::Nothing}} where {FT, TX, TY, TZ, G}
+
 #####
 ##### Advective fluxes
 #####
 
 # ccc, ffc, fcf
- @inline _viscous_flux_ux(i, j, k, ibg::GFIBG, args...) = conditional_flux_ccc(i, j, k, ibg, viscous_flux_ux, args...)
- @inline _viscous_flux_uy(i, j, k, ibg::GFIBG, args...) = conditional_flux_ffc(i, j, k, ibg, viscous_flux_uy, args...)
- @inline _viscous_flux_uz(i, j, k, ibg::GFIBG, args...) = conditional_flux_fcf(i, j, k, ibg, viscous_flux_uz, args...)
+ @inline _viscous_flux_ux(i, j, k, ibg::NoViscousFluxGFIBG, args...) = conditional_flux_ccc(i, j, k, ibg, viscous_flux_ux, args...)
+ @inline _viscous_flux_uy(i, j, k, ibg::NoViscousFluxGFIBG, args...) = conditional_flux_ffc(i, j, k, ibg, viscous_flux_uy, args...)
+ @inline _viscous_flux_uz(i, j, k, ibg::NoViscousFluxGFIBG, args...) = conditional_flux_fcf(i, j, k, ibg, viscous_flux_uz, args...)
  
  # ffc, ccc, cff
- @inline _viscous_flux_vx(i, j, k, ibg::GFIBG, args...) = conditional_flux_ffc(i, j, k, ibg, viscous_flux_vx, args...)
- @inline _viscous_flux_vy(i, j, k, ibg::GFIBG, args...) = conditional_flux_ccc(i, j, k, ibg, viscous_flux_vy, args...)
- @inline _viscous_flux_vz(i, j, k, ibg::GFIBG, args...) = conditional_flux_cff(i, j, k, ibg, viscous_flux_vz, args...)
+ @inline _viscous_flux_vx(i, j, k, ibg::NoViscousFluxGFIBG, args...) = conditional_flux_ffc(i, j, k, ibg, viscous_flux_vx, args...)
+ @inline _viscous_flux_vy(i, j, k, ibg::NoViscousFluxGFIBG, args...) = conditional_flux_ccc(i, j, k, ibg, viscous_flux_vy, args...)
+ @inline _viscous_flux_vz(i, j, k, ibg::NoViscousFluxGFIBG, args...) = conditional_flux_cff(i, j, k, ibg, viscous_flux_vz, args...)
  
  # fcf, cff, ccc
- @inline _viscous_flux_wx(i, j, k, ibg::GFIBG, args...) = conditional_flux_fcf(i, j, k, ibg, viscous_flux_wx, args...)
- @inline _viscous_flux_wy(i, j, k, ibg::GFIBG, args...) = conditional_flux_cff(i, j, k, ibg, viscous_flux_wy, args...)
- @inline _viscous_flux_wz(i, j, k, ibg::GFIBG, args...) = conditional_flux_ccc(i, j, k, ibg, viscous_flux_wz, args...)
+ @inline _viscous_flux_wx(i, j, k, ibg::NoViscousFluxGFIBG, args...) = conditional_flux_fcf(i, j, k, ibg, viscous_flux_wx, args...)
+ @inline _viscous_flux_wy(i, j, k, ibg::NoViscousFluxGFIBG, args...) = conditional_flux_cff(i, j, k, ibg, viscous_flux_wy, args...)
+ @inline _viscous_flux_wz(i, j, k, ibg::NoViscousFluxGFIBG, args...) = conditional_flux_ccc(i, j, k, ibg, viscous_flux_wz, args...)
 
 # fcc, cfc, ccf
-@inline _diffusive_flux_x(i, j, k, ibg::GFIBG, args...) = conditional_flux_fcc(i, j, k, ibg, diffusive_flux_x, args...)
-@inline _diffusive_flux_y(i, j, k, ibg::GFIBG, args...) = conditional_flux_cfc(i, j, k, ibg, diffusive_flux_y, args...)
-@inline _diffusive_flux_z(i, j, k, ibg::GFIBG, args...) = conditional_flux_ccf(i, j, k, ibg, diffusive_flux_z, args...)
+@inline _diffusive_flux_x(i, j, k, ibg::NoDiffusiveFluxGFIBG, args...) = conditional_flux_fcc(i, j, k, ibg, diffusive_flux_x, args...)
+@inline _diffusive_flux_y(i, j, k, ibg::NoDiffusiveFluxGFIBG, args...) = conditional_flux_cfc(i, j, k, ibg, diffusive_flux_y, args...)
+@inline _diffusive_flux_z(i, j, k, ibg::NoDiffusiveFluxGFIBG, args...) = conditional_flux_ccf(i, j, k, ibg, diffusive_flux_z, args...)
 
 #####
 ##### Advective fluxes
@@ -118,11 +127,6 @@ const GFIBG = ImmersedBoundaryGrid{FT, TX, TY, TZ, G, <:GridFittedBoundary} wher
 @inline near_x_boundary(i, j, k, ibg, ::AbstractAdvectionScheme{2}) = solid_cell(i - 2, j, k, ibg) || solid_cell(i - 1, j, k, ibg) || solid_cell(i, j, k, ibg) || solid_cell(i + 1, j, k, ibg) || solid_cell(i + 2, j, k, ibg)
 @inline near_y_boundary(i, j, k, ibg, ::AbstractAdvectionScheme{2}) = solid_cell(i, j - 2, k, ibg) || solid_cell(i, j - 1, k, ibg) || solid_cell(i, j, k, ibg) || solid_cell(i, j + 1, k, ibg) || solid_cell(i, j + 2, k, ibg)
 @inline near_z_boundary(i, j, k, ibg, ::AbstractAdvectionScheme{2}) = solid_cell(i, j, k - 2, ibg) || solid_cell(i, j, k - 1, ibg) || solid_cell(i, j, k, ibg) || solid_cell(i, j, k + 1, ibg) || solid_cell(i, j, k + 2, ibg)
-
-# Takes forever to compile, but works.
-# @inline near_x_boundary(i, j, k, ibg, ::AbstractAdvectionScheme{buffer}) where buffer = any(ntuple(δ -> solid_cell(i - buffer - 1 + δ, j, k, ibg), Val(2buffer + 1)))
-# @inline near_y_boundary(i, j, k, ibg, ::AbstractAdvectionScheme{buffer}) where buffer = any(ntuple(δ -> solid_cell(i, j - buffer - 1 + δ, k, ibg), Val(2buffer + 1)))
-# @inline near_z_boundary(i, j, k, ibg, ::AbstractAdvectionScheme{buffer}) where buffer = any(ntuple(δ -> solid_cell(i, j, k - buffer - 1 + δ, ibg), Val(2buffer + 1)))
 
 for bias in (:symmetric, :left_biased, :right_biased)
     for (d, ξ) in enumerate((:x, :y, :z))
