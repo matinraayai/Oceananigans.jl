120c7e26b2d8709a6497e5908c569b35be138a28
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-10-06 18:56:46 -0400

    AMD zeros

--- src/Grids/zeros.jl
+++ src/Grids/zeros.jl
@@ -1,9 +1,11 @@
 using CUDA
-using Oceananigans.Architectures: AbstractCPUArchitecture, AbstractGPUArchitecture
+using AMDGPU
+using Oceananigans.Architectures: AbstractCPUArchitecture, GPU, AMD
 
 import Base: zeros
 
 zeros(FT, ::AbstractCPUArchitecture, N...) = zeros(FT, N...)
-zeros(FT, ::AbstractGPUArchitecture, N...) = CUDA.zeros(FT, N...)
+zeros(FT, ::GPU, N...) = CUDA.zeros(FT, N...)
+zeros(FT, ::AMD, N...) = AMDGPU.zeros(FT, N...)
 
 zeros(arch::AbstractArchitecture, grid, N...) = zeros(eltype(grid), arch, N...)
