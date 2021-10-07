dcc227a855d926836f515dc1216ffac255017915
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-10-07 17:46:13 -0400

    `AMDGPU.zeros` acting weird so bypass it

--- src/Grids/zeros.jl
+++ src/Grids/zeros.jl
@@ -6,6 +6,6 @@ import Base: zeros
 
 zeros(FT, ::AbstractCPUArchitecture, N...) = zeros(FT, N...)
 zeros(FT, ::GPU, N...) = CUDA.zeros(FT, N...)
-zeros(FT, ::AMD, N...) = AMDGPU.zeros(FT, N...)
+zeros(FT, ::AMD, N...) = zeros(FT, N...) |> ROCArray # AMDGPU.zeros(FT, N...)
 
 zeros(arch::AbstractArchitecture, grid, N...) = zeros(eltype(grid), arch, N...)
